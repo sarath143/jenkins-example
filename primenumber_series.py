ad8fbea4991282f346556c11dc3830747f16b5e0
sarath143 <j.sarathchandra@gmail.com>
2020-01-02 15:17:18 +0800

    Update primenumber_series.py

--- primenumber_series.py
+++ primenumber_series.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import time
 def generate_numbers(number):
 	for num in range(1, number + 1):
    # all prime numbers are greater than 1
@@ -9,6 +10,7 @@ def generate_numbers(number):
 					break
 			else:
 				print(num)
+		time.sleep(3)
 
 number = int(sys.argv[1])
 generate_numbers(number)
